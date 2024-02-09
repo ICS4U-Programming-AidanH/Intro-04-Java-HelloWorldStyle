5b9bac6d58af1cc320a2ac14bc7098e3e41e68b2
Aidan Harvey <aidan.harvey@stu.ocsb.ca>
2024-02-09 15:29:06 +0000

    Version 1.0 of Intro-04-Java-HelloWorldStyle

---
+++ HelloWorldStyle.java
@@ -0,0 +1,25 @@
+/**
+
+* This HelloWorldStyle program is for Intro-04.
+
+* The code will output "Hello World!" when run.
+
+*
+
+* @author  Aidan Harvey
+
+* @version 1.0
+
+* @since   2024-02-09
+        year--month--day
+
+*/
+
+
+public class HelloWorldStyle {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //This will print "Hello World to the screen"
+    }
+    
+}
